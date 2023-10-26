91df983b05c698347e620b05a313630e6d875c93
OlegatorJava <olegator4895@gmail.com>
2023-10-26 11:29:22 +0500

    HW2

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,5 +3,11 @@ package org.example;
 public class HomeWork {
     public static void main(String[] args) {
         System.out.println("HomeWork");
+        System.out.println("HomeWork");
+        System.out.println("HomeWork");
+        System.out.println("HomeWork");
+        System.out.println("HomeWork");
+        System.out.println("HomeWork");
+        System.out.println("HomeWork");
     }
 }
